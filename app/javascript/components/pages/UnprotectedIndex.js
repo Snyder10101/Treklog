4a52dbe468aab6cef255c43809b38db1e37683bc
oagomezz <ogomezbarragan437@gmail.com>
2023-02-15 14:49:47 -0800

    fixed the errors in the pages

--- app/javascript/components/pages/UnprotectedIndex.js
+++ app/javascript/components/pages/UnprotectedIndex.js
@@ -7,17 +7,17 @@ import {
   CardSubtitle,
 } from "reactstrap";
 
-const UnprotectedIndex = ({trails}) => {
+const UnprotectedIndex = ({ trails }) => {
     return (
 <main>
-      {trails?.map((trails, index) => {
+      {trails?.map((trail, index) => {
         return (
           <Card color="light" style={{ width: "18rem"}} key={index}>
-            <img alt="Apartment Pic's" src={trails.image} />
+            <img alt="Trail Pic" src={trail.image} />
             <CardBody>
-              <CardTitle tag="h5">${trails.name}</CardTitle>
+              <CardTitle tag="h5">{trail.name}</CardTitle>
               <CardSubtitle className="mb-2 text-muted" tag="h6">
-                location {trails.location} Distance:{trails.distance}
+                location {trail.location} Distance:{trail.distance}
               </CardSubtitle>
               <Button>Learn More</Button>
             </CardBody>
