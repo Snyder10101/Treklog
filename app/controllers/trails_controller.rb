15810575773ecadfc14dd0b7269fbbaee56a24df
dgfarm02 <dgfarm2@gmail.com>
2023-02-15 16:33:29 -0800

    completed create endpoint backend card

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -3,4 +3,16 @@ class TrailsController < ApplicationController
         trails = Trail.all
         render json: trails
     end
+    def create
+        trail = Trail.create(trail_params)
+        if trail.valid?
+            render json: trail
+        else
+            rendr json: trail.errors, status:422
+        end
+    end
+    private
+    def trail_params
+        params.require(:trail).permit(:name, :location, :distance, :duration, :difficulty, :description, :image, :user_id)
+    end
 end
--- spec/requests/trails_spec.rb
+++ spec/requests/trails_spec.rb
@@ -20,4 +20,26 @@ RSpec.describe "Trails", type: :request do
       expect(trail.length).to eq(1)
     end
   end
+  describe "POST /create" do
+    it "creates a trail" do
+        user = User.where(email: 'willnotbereal@nawl.com').first_or_create(password: 'ABC123', password_confirmation: 'ABC123') 
+            trail_params = {
+                trail: {
+                  name: "test trail",
+                  location: "test location",
+                  distance: "test distance",
+                  duration: "test duration",
+                  difficulty: "test difficulty",
+                  description: "test description",
+                  image: "test.image",
+                  user_id: user.id
+                }}
+            post '/trails', params: trail_params
+            expect(response).to have_http_status(200)
+            trail=Trail.first
+            expect(trail.name).to eq "test trail"
+            expect(Trail.count).to eq 1
+           
+    end
+  end
 end
