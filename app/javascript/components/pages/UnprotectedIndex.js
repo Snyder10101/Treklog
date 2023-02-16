b0244d996f9a8cffb7dcacd068ba6348e4c0dc81
monicaarganda <argandamonica1@gmail.com>
2023-02-16 13:27:32 -0800

    added functionality to index page

--- app/javascript/components/pages/UnprotectedIndex.js
+++ app/javascript/components/pages/UnprotectedIndex.js
@@ -1,11 +1,6 @@
 import React from "react";
-import {
-  Card,
-  CardBody,
-  Button,
-  CardTitle,
-  CardSubtitle,
-} from "reactstrap";
+import { Card, CardBody, Button, CardTitle, CardSubtitle, } from "reactstrap";
+import { NavLink } from "react-router-dom"
 
 const UnprotectedIndex = ({ trails }) => {
     return (
@@ -19,7 +14,11 @@ const UnprotectedIndex = ({ trails }) => {
               <CardSubtitle className="mb-2 text-muted" tag="h6">
                 location {trail.location} Distance:{trail.distance}
               </CardSubtitle>
-              <Button>Learn More</Button>
+              <Button>
+                <NavLink to={`/show/${trail.id}`}>
+                  Learn More
+                </NavLink>
+                </Button>
             </CardBody>
           </Card>
         );
