d61a9610f64581dd4a1b7e14329a00d15bba1fdd
Christian Snyder <Christiansnyder562@gmail.com>
2023-02-21 16:27:52 -0800

    testing coverage done

--- app/javascript/components/pages/TrailNew.test.js
+++ app/javascript/components/pages/TrailNew.test.js
@@ -1,24 +1,61 @@
-import React from "react"
-import { render, screen } from "@testing-library/react"
-import TrailNew from "./TrailNew"
-import { BrowserRouter } from "react-router-dom"
+import React from "react";
+import { render, screen, fireEvent} from "@testing-library/react";
+import TrailNew from "./TrailNew";
+import { BrowserRouter, Router } from "react-router-dom";
+import { object } from "prop-types";
+import '@testing-library/jest-dom'
 
 describe("<TrailNew />", () => {
+  const current_user = {
+    id: 1,
+    name: "John Doe",
+  };
   it("renders without crashing", () => {
-    const div = document.createElement("div")
-    render(  
-    <BrowserRouter>
-      <TrailNew />
-    </BrowserRouter>)
-  })
+    const div = document.createElement("div");
+    render(<TrailNew />, div);
+  });
   it("can see the title of the page", () => {
-    const div = document.createElement("div")
+    const div = document.createElement("div");
+    render(<TrailNew />, div);
+    const title = screen.getByText("Create", { exact: false });
+    expect(title.textContent).toEqual("Create a new Trail");
+  });
+  it("has a button", () => {
     render(
       <BrowserRouter>
         <TrailNew />
       </BrowserRouter>
-      )
-    const title = screen.getByText("Create", {exact: false})
-    expect(title.textContent).toEqual("Create a new Trail")
+    );
+    const button = screen.getByRole("button", {
+      name: /Add Trail/i,
+    });
+    expect(button).toBeInTheDocument();
+  });
+  it("has a form with all entries", () => {
+    render(
+      <BrowserRouter>
+        <TrailNew currentUser={current_user} />
+      </BrowserRouter>
+    );
+    const formName = screen.getByText("Name" ,{exact: false})
+    expect(formName.getAttribute("for")).toEqual("trail name")
+    const formLocation = screen.getByText(/location/i)
+    expect(formLocation.getAttribute("for")).toEqual("location")
+    const formImage = screen.getByText(/image/i)
+    expect(formImage.getAttribute("for")).toEqual("image")
   })
-})
+  it("calls the createTrail function and navigates to '/trailsindex' when the 'Add Trail' button is clicked", () => {
+    const createTrail = jest.fn("hello")
+    const navigate = jest.fn("hello")
+    const currentUser = "user123"
+    const { getByText, getByPlaceholderText } = render(<BrowserRouter >
+    <TrailNew createTrail={createTrail} currentUser={currentUser} /> </BrowserRouter>)
+    fireEvent.change(getByPlaceholderText("Type the trail name"), { target: { value: "Test Trail" } })
+    fireEvent.change(getByPlaceholderText("Input the location"), { target: { value: "Test Location" } })
+    fireEvent.change(getByPlaceholderText("How many miles?"), { target: { value: "5" } })
+    fireEvent.change(getByPlaceholderText("How long was the hike?"), { target: { value: "2 hours" } })
+    fireEvent.change(getByPlaceholderText("Beginner, Intermediate, Advanced"), { target: { value: "Beginner" } })
+    fireEvent.change(getByPlaceholderText("Description"), { target: { value: "Test Description" } })
+    fireEvent.change(getByPlaceholderText("image"), { target: { value: "https://example.com/image.jpg" } })
+  })
+});
